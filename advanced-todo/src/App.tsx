cd48870fe29600fa417cb8241c37d9c25e787b00
Rahman Ahmed <rahman2782@gmail.com>
2025-08-19 03:35:19 -0400

    added day1 notes

--- advanced-todo/src/App.tsx
+++ advanced-todo/src/App.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import './App.css';
 
+let name: string; //strings
+let age: number | string; //union, age can be a num or string
+name = 'Rahman';
+age = 23;
+let hobbies: string[]; //string array
+let job: [number, string]; //tuple - contains one num and one string value
+
+type Person = {
+  name: string;
+  age?: number; //optional
+  job?: [number, string]; //optional
+}
+
+let deliClerk: Person = {
+  name: "Steven",
+}
+
+
+
 function App() {
   return (
     <div className="App">
